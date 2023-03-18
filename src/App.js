956135d9d96fd1c450099668091bd52d4bb7f3d5
Apungu Raymond <apunguray@gmail.com>
2023-03-18 18:54:51 +0000

    commit update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Layout1 from "./components/Layout1";
+import Layout2 from "./components/Layout2";
 
 const App = () => {
   return (
-    <Layout1 />
+    <Layout2 />
   );
 };
 
