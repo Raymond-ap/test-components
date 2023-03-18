a2edd71f69eddf133fa5526596a36d4ed281b0c8
Apungu Raymond <apunguray@gmail.com>
2023-03-18 18:56:23 +0000

    local update

--- src/components/Layout2.js
+++ src/components/Layout2.js
@@ -142,7 +142,7 @@ function DropdownButton({ options }) {
         </button>
       </div>
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
           <div className="py-1">
             {options.map((option) => (
               <button
